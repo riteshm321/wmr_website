53103e80cc2298f1e40bd51d1c9bf473760e82e9
Javed Attar <javed.attar@miba.com>
2023-07-10 10:45:05 +0530

    category bug resolved

--- WisdomMarketResearch/settings.py
+++ WisdomMarketResearch/settings.py
@@ -66,6 +66,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'report.context_processors.getPublisherCategory'
             ],
         },
     },
@@ -122,7 +123,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')] # use in test
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 
 MEDIA_URL = '/media/'
